2f0c5c17da3a14a4670ec78ada1562f845e385db
DANIEL ROBSON ALEXANDRE SILVA <daniel.silva067@cs.cruzeirodosul.edu.br>
2025-09-16 19:44:32 -0300

    Aula 6, tipos de condições

---
+++ Teste2.java/src/aula 6/ForExample.java
@@ -0,0 +1,10 @@
+public class ForExample {
+    public static void main(String[] args) {
+        int x;
+        for (x = 0; x < 10; x = x +1 ){
+            System.out.println(" X vale no for: "+ x);
+        }
+
+    }
+    
+}
---
+++ Teste2.java/src/aula 6/WhileExample.java
@@ -0,0 +1,10 @@
+public class WhileExample {
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        while (x < 10) {
+            System.out.println("X vale: " + x);
+            x = x + 1;
+        }
+    }
+}
